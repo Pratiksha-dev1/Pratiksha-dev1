e98b3951e5b05d9074dc98bc874b62d4559c8776
Pratiksha-dev1 <prthakare.thakare@gmail.com>
2025-05-11 18:56:29 +0000

    button clik

--- JS Projects/tipCalculator/tipCal.js
+++ JS Projects/tipCalculator/tipCal.js
@@ -1,5 +1,7 @@
 let billAmount = document.getElementById("bill");
 let tipAmount = document.getElementById("tip");
+const buntevent = document.getElementById("calculate");
 function calculateTip() {
     let tip = billAmount.value * (1 + (tipAmount.value / 100));
 }
+buntevent.addEventListener("click", calculateTip);
