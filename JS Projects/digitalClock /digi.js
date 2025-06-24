a044f0c5ee451eb44a3a6a7e6e2b23455f8aa347
Pratiksha-dev1 <prthakare.thakare@gmail.com>
2025-06-24 11:45:11 +0000

    am/pm and 24 digit

--- JS Projects/digitalClock /digi.js
+++ JS Projects/digitalClock /digi.js
@@ -4,11 +4,31 @@ let date = new Date();
 let h = date.getHours();
 let m = date.getMinutes();
 let s = date.getSeconds(); 
+let session;
+
+if(h == 12)
+{
+    h=0;
+    session = "AM";
+}
+
+if(h > 12)
+{
+     h -= 12;
+     session = "PM";
+
+}
+
+h = (h<10) ? h= "0"+ h : h;
+m = (m<10) ? m= "0"+ m : m;
+s = (s<10) ? s= "0"+ s : s; 
 
 let screenTime = `${h} : ${m} : ${s}`;
 console.log(screenTime);
-
-
 document.getElementById("time").innerHTML = screenTime;
+
+setTimeout(showTime, 1000);
 }
+
+showTime();
 
