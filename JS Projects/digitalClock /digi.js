ae04ebc06ef434f987ec5ed46b98a854d8776128
Pratiksha-dev1 <prthakare.thakare@gmail.com>
2025-06-24 11:50:42 +0000

    added session

--- JS Projects/digitalClock /digi.js
+++ JS Projects/digitalClock /digi.js
@@ -23,7 +23,7 @@ h = (h<10) ? h= "0"+ h : h;
 m = (m<10) ? m= "0"+ m : m;
 s = (s<10) ? s= "0"+ s : s; 
 
-let screenTime = `${h} : ${m} : ${s}`;
+let screenTime = `${h} : ${m} : ${s}  ${session}`;
 console.log(screenTime);
 document.getElementById("time").innerHTML = screenTime;
 
